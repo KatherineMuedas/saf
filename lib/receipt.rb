a33e2f0e7a07f4f9ce6e15e2a4a3aeb0eeec81f0
katherine <muedasnka@gmail.com>
2019-03-11 15:46:31 -0400

    receive user input and clean up input

--- lib/receipt.rb
+++ lib/receipt.rb
@@ -1,5 +1,34 @@
+require 'pry'
 class Receipt
+  #Receive input from user
+  #Convert it into an array
+
+  def menu
+    puts "What would you like to order?"
+    input = gets.chomp.split
+
+    unless input.size < 3
+      clean_input(input)
+      p input
+    else
+      puts 'NOT ENOUGH INFORMARTION. Enter quantity, product and price. Example: 2 book at 12.49.'
+      menu
+    end
+  end
+
+
+  private
+
+  def clean_input(input)
+   input.delete( 'at') if input.include?('at')
+  end
+
+
+
+
+
+
 
 end
 
-Receipt.new
+Receipt.new.menu
