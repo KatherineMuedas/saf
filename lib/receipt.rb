85e492825f60c5a124451ab4f507ad8e544855dd
katherine <muedasnka@gmail.com>
2019-03-11 18:50:06 -0400

    set sales_tax and import duty per item, calculate taxes values and add them to requested line items

--- lib/receipt.rb
+++ lib/receipt.rb
@@ -5,6 +5,7 @@ require 'pry'
   #save details in a hash
   #add requested line items into a basket
   #print receipt base on basket information
+
 class Receipt
   def initialize
     @basket = []
@@ -28,12 +29,23 @@ class Receipt
   def set_requested_line_items_details(input)
     item_name = input[1..-2].join(" ")
     item_price = input[-1].to_f.round(2)
+    item_name.include?('chocolate') || item_name.include?('book') || item_name.include?('pill') ? sales_tax = 0 : sales_tax = 0.1
+    item_name.include?('imported') ? import_duty = 0.05 : import_duty = 0
+    item_total_taxes = sales_tax_rounding((item_price * sales_tax) + (item_price * import_duty))
+    item_total_price = (item_price + item_total_taxes).round(2)
+
     quantity = input[0].to_i
+    line_items_total_price = (item_total_price * quantity).round(2)
+    line_items_total_taxes = (item_total_taxes * quantity).round(2)
 
     @requested_line_items = {
       item_name: item_name,
-      item_price: item_price,
-      quantity: quantity
+      sales_tax: sales_tax,
+      import_duty: import_duty,
+      quantity: quantity,
+      item_total_taxes: item_total_taxes,
+      line_items_total_price: line_items_total_price,
+      line_items_total_taxes: line_items_total_taxes
     }
   end
 
@@ -45,9 +57,11 @@ class Receipt
     puts "************************"
     puts "************************"
     puts "RECEIPT"
-    @basket.each do |line_item|
-      puts "#{line_item[:quantity]} #{line_item[:item_name]}: #{line_item[:item_price]}"
+    basket.each do |line_item|
+      puts "#{line_item[:quantity]} #{line_item[:item_name]}: #{line_item[:line_items_total_price]}"
     end
+    puts "Sales Taxes:#{basket.map{|line_item| line_item[:line_items_total_taxes]}.inject(&:+).round(2)}"
+    puts "Total: #{basket.map{|line_item| line_item[:line_items_total_price]}.inject(&:+).round(2)}"
     puts "************************"
     puts "************************"
   end
@@ -56,6 +70,10 @@ class Receipt
 
   def clean_input(input)
    input.delete( 'at') if input.include?('at')
+  end
+
+  def sales_tax_rounding(taxes_amount)
+    (taxes_amount * 20).ceil / 20.00
   end
 
 end
